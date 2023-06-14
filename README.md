29628eeff7c74cc081c1253eebe172a2b1cc0978
NaranjaX <naranjax@MAC-67555.local>
2023-06-14 16:48:46 -0300

    init project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# gestionRepuestos
+# Ejecutar en desarrollo
+
+1. Clonar el repositorio
+2. Ejecutar
+```
+npm install
+```
+5. Clonar el archivo ```.env.template``` y renombrar la copia a ```.env```
+
+6. Llenar las variables de entorno definidas en el ```.env```
+
+7. Ejecutar la aplicación en dev:
+```
+npm run dev
+```
+## Stack usado
+* Typescript
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express, { Express } from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+import carBrandRoute from './routes/carBrands'
+import categorieRoute from './routes/categories'
+import productBrandRoute from './routes/productBrands'
+import productRoute from './routes/products'
+import providerRoute from './routes/providers'
+
+dotenv.config()
+
+const app: Express = express()
+
+// Middlewares
+app.use(express.json()) // transforma la req.body a un json
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+// app.use(express.static(__dirname + '/public'))
+
+app.get('/', (_req, res) => {
+  res.send('Bienvenido!!!!')
+})
+
+const PATH = '/api/'
+
+app.use(`${PATH}car-brands`, carBrandRoute)
+app.use(`${PATH}categories`, categorieRoute)
+app.use(`${PATH}product-brands`, productBrandRoute)
+app.use(`${PATH}product`, productRoute)
+app.use(`${PATH}providers`, providerRoute)
+
+const PORT = process.env.PORT ?? 8080
+const server = app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running on port ${PORT}`)
+})
+server.on('error', error => console.log('Server error', error))
