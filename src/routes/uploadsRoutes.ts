56e4c9688383f2944b2fc55c3ab7b656348bc677
Marcos Verblud <marcosverblud@gmail.com>
2023-08-08 16:20:52 -0300

    add validated JWT and Role

--- src/routes/uploadsRoutes.ts
+++ src/routes/uploadsRoutes.ts
@@ -1,19 +1,17 @@
 import express from "express";
 import upload from "../config/uploadConfig";
 import { updatePrice, uploadCSV } from "../controllers/uploadsController";
+import validateJWT from "../middlewares/validateJWT";
+import isAdminRole from "../middlewares/isAdminRole";
 
 const router = express.Router();
 
-router.post(
-  "/",
-  [
-    //validarJWT,
-    //esAdminRole,
-    upload.single("file"),
-  ],
-  uploadCSV
-);
+router.post("/", [validateJWT, isAdminRole, upload.single("file")], uploadCSV);
 
-router.post("/update-prices/:id", [upload.single("file")], updatePrice);
+router.post(
+  "/update-prices/:id",
+  [validateJWT, isAdminRole, upload.single("file")],
+  updatePrice
+);
 
 export default router;
